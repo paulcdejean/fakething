fcffe2cd375d16e9ab7c23da1d617254f9f4bed9
Paul Dejean <paulcdejean@gmail.com>
2015-03-04 12:38:58 +0000

    Geez

--- python_script.py
+++ python_script.py
@@ -3,3 +3,16 @@ import boto
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 print "Hello boto"
+
+result = {
+    "project": "twb-firmware",
+    "branch": "",
+    "commit": "",
+    "urls": []
+    }
+
+result["project"] = os.environ["CI_PROJECT"]
+result["branch"] = os.environ["CI_BRANCH"]
+result["commit"] = os.environ["CI_COMMIT_ID"]
+
+print "This doesn't work"
